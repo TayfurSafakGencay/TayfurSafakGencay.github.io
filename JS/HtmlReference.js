e59219ee2933322bf81cb07c96a8dda21b91e813
TayfurSafakGencay <safak.gencay@gmail.com>
2024-08-13 23:42:38 +0300

    New: About Me

---
+++ JS/HtmlReference.js
@@ -0,0 +1,24 @@
+﻿// main.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // İçerik dosyalarının yollarını tanımlayın
+    const sections = {
+        'about': '../HTML/Section/About.html',
+        // 'services': 'services.html',
+        // 'portfolio': 'portfolio.html',
+        // 'contact': 'contact.html'
+    };
+
+    // Her section için içerik yükleme işlemi
+    Object.keys(sections).forEach(id => {
+        const section = document.getElementById(id);
+        if (section) {
+            fetch(sections[id])
+                .then(response => response.text())
+                .then(data => {
+                    section.innerHTML = data;
+                })
+                .catch(error => console.error('Error loading content:', error));
+        }
+    });
+});
--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
-﻿function openModal(modalId) {
-    document.getElementById(modalId).style.display = "flex";
+﻿window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    const mainNav = document.getElementById('main-nav');
+    const stickyNav = document.querySelector('.sticky-navbar');
+    const stickyNavHeight = stickyNav.offsetHeight;
+
+    if (window.scrollY > header.offsetHeight) {
+        stickyNav.style.display = 'flex';
+        mainNav.style.display = 'none';
+        document.body.classList.add('sticky-nav');
+    } else {
+        stickyNav.style.display = 'none';
+        mainNav.style.display = 'flex';
+        document.body.classList.remove('sticky-nav');
+    }
+});
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'flex';
 }
 
 function closeModal(modalId) {
-    document.getElementById(modalId).style.display = "none";
+    document.getElementById(modalId).style.display = 'none';
 }

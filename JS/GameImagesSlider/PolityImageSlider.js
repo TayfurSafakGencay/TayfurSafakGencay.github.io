c558d3eba5b9ff73f68e731a9d27a9e6cb84343b
TayfurSafakGencay <safak.gencay@gmail.com>
2024-08-21 09:31:35 +0300

    New: New projects added.

---
+++ JS/GameImagesSlider/PolityImageSlider.js
@@ -0,0 +1,22 @@
+﻿let currentSlide = 0;
+
+const images = [
+    "Images/GameContents/Polity/PolityFarming.jpg",
+    "Images/GameContents/Polity/PolityJobs.jpg",
+    "Images/GameContents/Polity/PolityDisco.jpg",
+    "Images/GameContents/Polity/ClothGif.gif"
+];
+
+function showSlide(index) {
+    const sliderImage = document.getElementById('slider-image');
+    currentSlide = (index + images.length) % images.length;
+    sliderImage.src = images[currentSlide];
+}
+
+function changeSlide(direction) {
+    showSlide(currentSlide + direction);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    showSlide(currentSlide); // İlk resmi göster
+});
